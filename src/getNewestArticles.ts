374c259b8f9264fb5cb6e345f2024c809410d6ba
Dell-PC-KA-Raouf <ka_raouf@outlook.com>
2024-11-11 15:13:04 +0800

    Enhance Puppeteer configuration in getNewestArticles function and add logging in index.html

--- src/getNewestArticles.ts
+++ src/getNewestArticles.ts
@@ -1,38 +1,42 @@
 import puppeteer from "puppeteer";
 
 export async function getNewestArticles(): Promise<string[]> {
-    const websiteURL = "https://www.algerie360.com/tag/a-la-une/";
-    // Launch a new browser instance
-    const browser = await puppeteer.launch();
-    // Open a new page
-    const page = await browser.newPage();
-    // Navigate to the specified URL
-    await page.goto(websiteURL, { waitUntil: "networkidle2" });
-  
-    // Extract all hyperlinks from <article> elements on the page
-    const articleHyperlinks = await page.evaluate(() => {
-      // Find the div with the specified class
-      const targetDiv = Array.from(
-        document.querySelectorAll("div.col-lg-8.blog__content.mb-72")
-      )[0];
-  
-      const links: string[] = [];
-      if (targetDiv) {
-        // Select all <a> elements inside the targetDiv
-        const anchorElements = targetDiv.querySelectorAll("article a");
-        anchorElements.forEach((anchor) => {
-          const href = anchor.getAttribute("href");
-          if (href) links.push(href);
-        });
-      }
-      // Filter out duplicate links and links that include the word "category"
-      return Array.from(
-        new Set(links.filter((link) => !link.includes("category")))
-      );
-    });
-  
-    // Close the browser
-    await browser.close();
-  
-    return articleHyperlinks;
-  }
+  const websiteURL = "https://www.algerie360.com/tag/a-la-une/";
+  // Launch a new browser instance
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+    executablePath:
+      process.env.PUPPETEER_EXECUTABLE_PATH || "/usr/bin/chromium-browser",
+  });
+  // Open a new page
+  const page = await browser.newPage();
+  // Navigate to the specified URL
+  await page.goto(websiteURL, { waitUntil: "networkidle2" });
+
+  // Extract all hyperlinks from <article> elements on the page
+  const articleHyperlinks = await page.evaluate(() => {
+    // Find the div with the specified class
+    const targetDiv = Array.from(
+      document.querySelectorAll("div.col-lg-8.blog__content.mb-72")
+    )[0];
+
+    const links: string[] = [];
+    if (targetDiv) {
+      // Select all <a> elements inside the targetDiv
+      const anchorElements = targetDiv.querySelectorAll("article a");
+      anchorElements.forEach((anchor) => {
+        const href = anchor.getAttribute("href");
+        if (href) links.push(href);
+      });
+    }
+    // Filter out duplicate links and links that include the word "category"
+    return Array.from(
+      new Set(links.filter((link) => !link.includes("category")))
+    );
+  });
+
+  // Close the browser
+  await browser.close();
+
+  return articleHyperlinks;
+}
