13ef54e18557ae2eb544541b9737e71a21c3b59e
Dell-PC-KA-Raouf <ka_raouf@outlook.com>
2025-02-09 20:47:59 +0800

    Update axios version in package.json; modify uploadThumbnailImage to use httpsAgent for secure requests

--- src/uploadThumbnailImage.ts
+++ src/uploadThumbnailImage.ts
@@ -1,18 +1,17 @@
 import * as fs from "fs";
 import * as FormData from "form-data";
 import axios from "axios";
-import { globalAgent } from "http";
 
 export async function uploadThumbnailImage(filePath: string): Promise<any> {
     const form = new FormData.default();
     form.append("media", fs.createReadStream(filePath));
-  
+    
     const uploadResponse: any = await axios.post(
       `https://api.weixin.qq.com/cgi-bin/material/add_material?type=image`,
       form,
       {
         headers: form.getHeaders(),
-        auth: new (require('https').Agent)({ rejectUnauthorized: false })
+        httpsAgent: new (require('https').Agent)({ rejectUnauthorized: false })
       }
     );
   
